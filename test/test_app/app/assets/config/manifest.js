b1ac8d2cf886b96b0328565b882779154f3aab2f
BradPotts <brad.potts@phcnet.co>
2019-10-15 11:31:37 +0000

    updated manifest.

--- test/test_app/app/assets/config/manifest.js
+++ test/test_app/app/assets/config/manifest.js
@@ -3,3 +3,5 @@
 //= link phcdevworks_scripts_manifest.js
 //= link phcdevworks_accounts/application.css
 //= link phcdevworks_accounts/application.js
+//= link phcdevworks_scripts/application.css
+//= link phcdevworks_scripts/application.js
